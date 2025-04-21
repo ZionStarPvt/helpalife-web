48ebbee343d5b04a8e1ca0ab496e9a0b45bc06f1
Ajinslin <ajinslin@zionstar.in>
2025-04-21 15:57:02 +0530

    Implement home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root "application#home"
+  get "home",to:'home#index'
+  root to: 'home#index'
   get 'home', to:'application#home'
   get 'donors', to: 'donor#index'
   devise_scope :user do
