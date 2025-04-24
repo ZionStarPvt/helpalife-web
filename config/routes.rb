c13d8fd63191ddc3f792b4ea9d50364deda9d6e0
Ajinslin <ajinslin@zionstar.in>
2025-04-24 15:09:22 +0530

    implement donations page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'home', to:'application#home'
   get 'donors', to: 'donor#index'
   get "about",to: 'home#about'
+  get "donations",to:'home#donations'
   devise_scope :user do
     devise_for :users, controllers: { registrations: 'users/registrations' }
     get 'register', to: 'users/registrations#new'
