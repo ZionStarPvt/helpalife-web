7507f75c4f6f604e1e93dac1a23c9b7311812913
Ajinslin <ajinslin@zionstar.in>
2025-05-07 16:36:13 +0530

    login page

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,13 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+import "@hotwired/turbo-rails"
+
+import * as bootstrap from "bootstrap"
+
+// Re-initialize dropdowns after Turbo loads
+document.addEventListener('turbo:load', () => {
+    document.querySelectorAll('[data-bs-toggle="dropdown"]').forEach((dropdownToggleEl) => {
+        new bootstrap.Dropdown(dropdownToggleEl)
+    })
+})
